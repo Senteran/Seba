1301f0a8167d90ab6713154f8d620121122b276f
Senteran <43209971+Senteran@users.noreply.github.com>
2021-06-01 17:07:36 +0200

    Wiecej spritow

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     pygame.init()
 
     image = pygame.image.load('seba.png')
+    images = [pygame.image.load('cl.png'), pygame.image.load('erty.png'), pygame.image.load('exeos.png'), pygame.image.load('hot.png'), pygame.image.load('kfra.png'), pygame.image.load('krup.png'), pygame.image.load('seba.png'), pygame.image.load('sent.png')]
     pygame.display.set_icon(image)
     pygame.display.set_caption('Seba!')
 
@@ -12,7 +13,7 @@ def main():
 
     running = True
 
-    coords = [10, 10]
+    coords = [[10, 10], [300, 10], [10, 300], [150, 150], [250, 100], [1000, 10], [10, 500], [400, 400]]
 
     while running:
         for event in pygame.event.get():
@@ -20,20 +21,20 @@ def main():
                 running = False
         
         screen.fill((50,50,50))
-        screen.blit(image, (coords[0], coords[1]))
 
-        ran = random.randint(1, 10)
+        for i in range(8):
+            screen.blit(images[i], (coords[i][0], coords[i][1]))
+            ran = random.randint(1, 10)
 
-        if(ran < 4):
-            coords[1] = coords[1] + 1
-        else:
-            coords[0] = coords[0] + 1
-        
-        if(coords[0] > 1150):
-            coords[0] = 0
-        else: 
-            if(coords[1] > 600):
-                coords[1] = 10
+            if(ran < 4):
+                coords[i][1] = coords[i][1] + 1
+            else:
+                coords[i][0] = coords[i][0] + 1
+            
+            if(coords[i][0] > 1150):
+                coords[i][0] = 10
+            if(coords[i][1] > 600):
+                coords[i][1] = 10
 
         pygame.display.flip()
 
