9472b16b0daca23452ff6d8800926455068db170
Senteran <43209971+Senteran@users.noreply.github.com>
2021-06-01 17:22:45 +0200

    Wiele stron

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     running = True
 
     coords = [[10, 10], [300, 10], [10, 300], [150, 150], [250, 100], [1000, 10], [10, 500], [400, 400]]
+    directions = [[True, True], [True, True], [True, True], [True, True], [True, True], [True, True], [True, True], [True, True]]
 
     while running:
         for event in pygame.event.get():
@@ -25,16 +26,33 @@ def main():
         for i in range(8):
             screen.blit(images[i], (coords[i][0], coords[i][1]))
             ran = random.randint(1, 10)
+            dir_change = random.randint(1, 1000)
+            if dir_change == 50:
+                directions[i][0] = not directions[i][0]
+            if dir_change == 51:
+                directions[i][1] = not directions[i][1]
+            if directions[i][0]:
+                dir_modifier_x = 1
+            else:
+                dir_modifier_x = -1
+            if directions[i][1]:
+                dir_modifier_y = 1
+            else:
+                dir_modifier_y = -1
 
             if(ran < 4):
-                coords[i][1] = coords[i][1] + 1
+                coords[i][1] = coords[i][1] + dir_modifier_y
             else:
-                coords[i][0] = coords[i][0] + 1
+                coords[i][0] = coords[i][0] + dir_modifier_x
             
-            if(coords[i][0] > 1150):
+            if(coords[i][0] > 1100):
                 coords[i][0] = 10
             if(coords[i][1] > 600):
                 coords[i][1] = 10
+            if(coords[i][0] < 0):
+                coords[i][0] = 1100
+            if(coords[i][1] < 0):
+                coords[i][1] = 600
 
         pygame.display.flip()
 
